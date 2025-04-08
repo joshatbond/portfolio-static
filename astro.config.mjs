e6c3bd36f5017f91a57fe4a5f7a4face1dcd3fac
Joshua Richardson <jrichardson@wearebond.com>
2025-04-07 17:53:00 -0700

    Update (#1)
    * chore(upgrade): Update astro, react, and tailwind to latest available. Reworks tailwindcss for v4
    
    * fix(cv): move resume to the top navbar
    
    * feat(page): create a rotating gradient for visual interest
    
    * fix(layout): improve style consistency across light/dark theme
    
    * feat(theme): implement light/dark theming
    
    * fix(theme): properly manage toggle checked state
    
    * fix(nav): cleanup hover states
    
    * fix(data): remove portfolio site
    
    * fix(layout): header should always be visible
    
    * fix(style): don't clobber tailwind variants
    
    * fix(layout): move sizing of element to the layout to separate concerns
    
    * fix(homepage): Update text and placement
    
    * feat(cards): add project cards in a gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig(
   {
-    integrations: [react(), tailwind({ applyBaseStyles: false })],
+    integrations: [react()],
+    vite: {
+      plugins: [tailwindcss()],
+    },
   } // ...
 )
