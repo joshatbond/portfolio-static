aed3234ab996bd29d2fd7030ed96a4039edd58c1
Joshua Richardson <jrichardson@wearebond.com>
2025-04-15 15:56:55 -0700

    Svg sprite (#3)
    * feat(svg-sprite): implement plugin
    
    * fix(svg-sprite): fix sprite loading
    
    * feat(sprite): implement svg sprite
    
    * fix(layout): change to a resizeObserver to detect when header changes sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@ import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import svgSprite from './plugins/vite-plugin-svg-sprite'
+
 // https://astro.build/config
 export default defineConfig(
   {
     integrations: [react()],
     vite: {
-      plugins: [tailwindcss()],
+      plugins: [
+        tailwindcss(),
+        svgSprite({
+          inputDir: 'src/sprites',
+          dtsOutputFile: 'src/svg-sprite.d.ts',
+        }),
+      ],
     },
   } // ...
 )
